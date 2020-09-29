2a50b60e268ce5055d1a28630b6523b832c22265
Furrazlhf <brian.lecarpentier@edu.itescia.fr>
2020-09-29 11:52:32 +0200

    summarize project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+// Import the dependencies and necessary modules
+import Vue from 'vue';
+import App from './App.vue';
+import FusionCharts from 'fusioncharts';
+import Charts from 'fusioncharts/fusioncharts.charts';
+import Widgets from 'fusioncharts/fusioncharts.widgets';
+import PowerCharts from 'fusioncharts/fusioncharts.powercharts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+import VueFusionCharts from 'vue-fusioncharts';
+import { store } from '@/store'
 
-Vue.config.productionTip = false
+Charts(FusionCharts);
+PowerCharts(FusionCharts);
+Widgets(FusionCharts);
+FusionTheme(FusionCharts);
+
+Vue.use(VueFusionCharts, FusionCharts);
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
+  store
 }).$mount('#app')

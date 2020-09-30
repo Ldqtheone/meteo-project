a8759cbb0033d77b058f03a9d347db924d3dee68
Furrazlhf <brian.lecarpentier@edu.itescia.fr>
2020-09-30 14:21:56 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,5 @@
 const API_KEY = "fdf81dbac6b7b0e2d531f26f7172eb2d";
 const CURRENT_WEATHER_CITY = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
 const CURRENT_WEATHER_UV = `http://api.openweathermap.org/data/2.5/uvi?appid=${API_KEY}`;
-const CURRENT_WEATHER_CLIMATE = `https://api.openweathermap.org/data/2.5/forecast/climate?appid=${API_KEY}`;
 
-export {API_KEY, CURRENT_WEATHER_CITY, CURRENT_WEATHER_UV, CURRENT_WEATHER_CLIMATE};
+export {API_KEY, CURRENT_WEATHER_CITY, CURRENT_WEATHER_UV};

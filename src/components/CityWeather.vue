73c20fda090afee8df55c2ea47dc7986985c4e22
Furrazlhf <brian.lecarpentier@edu.itescia.fr>
2020-10-02 09:55:55 +0200

    Revert "Update CityWeather.vue"
    This reverts commit 05229be4d37cf5fa7e12de488f991fd06d7ef653.

--- src/components/CityWeather.vue
+++ src/components/CityWeather.vue
@@ -43,6 +43,9 @@
             <span>Temperature : {{ cityN.main.temp | celciusF }}</span>
             <br/>
             <span>Vitesse du vent : {{ [cityN.wind.speed] | windS }}</span>
+            <!--
+        <UvValue v-bind:uvValue="displayUv" />
+  -->
             <br />
             <span
             >Humidité : {{ cityN.main.humidity }}%
@@ -108,6 +111,8 @@
 
 <script>
 
+/*import { CURRENT_WEATHER_UV } from "../constants";*/
+
 import {CURRENT_WEATHER_CITY} from "../constants";
 import axios from "axios";
 
@@ -139,7 +144,33 @@ export default {
             this.$store.commit("deleteCity", response.data);
           });
     },
-  },
+    },
+    /*displayUv: {
+      get() {
+        this.lat = this.$store.getters.cityInfos.map((item) => {
+          return item.coord.lat;
+        });
+
+        this.lon = this.$store.getters.cityInfos.map((item) => {
+          return item.coord.lon;
+        });
+
+        return axios
+            .get(CURRENT_WEATHER_UV + "&lat=" + this.lat + "&lon=" + this.lon)
+            .then((response) => {
+              console.log(response.data);
+              return response.data.value;
+            })
+            .catch((e) => {
+              this.errors.push(e);
+              this.$store.commit("displayError", e);
+            });
+      },
+      default() {
+        return "Loading";
+      },
+    }
+     */
   filters: {
     celciusF: (value) => {
       return (
